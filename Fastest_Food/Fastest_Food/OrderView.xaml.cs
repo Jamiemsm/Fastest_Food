7856988c049d14c98f1c8add7fd7846dce766685
Christoffer Holdgaard <chri810q@student.eadania.dk>
2019-02-05 12:23:53 +0100

    ORDERVIEW

--- Fastest_Food/Fastest_Food/OrderView.xaml.cs
+++ Fastest_Food/Fastest_Food/OrderView.xaml.cs
@@ -10,19 +10,23 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Fastest_Food
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
-			InitializeComponent();
-		}
-	}
+    /// <summary>
+    /// Interaction logic for OrderView.xaml
+    /// </summary>
+    public partial class OrderView : Window
+    {
+        public OrderView()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_OrderView_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
 }
